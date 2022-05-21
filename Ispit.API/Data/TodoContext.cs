5519bd1cec2fb4049176dfa0c1c302915668ced0
Dorian Ribarić <dorian.ribaric@live.com>
2022-05-21 02:03:49 +0200

    Created database todo_db with EF

---
+++ Ispit.API/Data/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Ispit.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ispit.API.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        { }
+
+        public DbSet<Todo> TodoList { get; set; }
+
+
+    }
+}
---
+++ Ispit.API/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ispit.API.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Exceeded maximum Title length of 100 characters")]
+        public string Title { get; set; }
+
+        [StringLength(3000, ErrorMessage = "Exceeded maximum description length")]
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; } = false;
+
+    }
+}
